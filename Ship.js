e278d1a6dfcd0336063651a4bb688dff4c7dc49a
gniht <jasonthomasjat@gmail.com>
2021-11-24 10:03:23 -0800

    refactored isSunk

--- Ship.js
+++ Ship.js
@@ -2,12 +2,17 @@
 class Ship {
   constructor( size ) {
     this.size = size;
+    this.hullIntegrity = [];
+    for(let i = 0; i < size; i++){
+      this.hullIntegrity.push(true);
+    }    
   }
   hit( hitLocation ) {
 
   }
   isSunk(){
-    return false;
+    return this.hullIntegrity.indexOf(true) < 0;
+    console.log(this.hullIntegrity);
   }
 }
 
--- Ship.test.js
+++ Ship.test.js
@@ -1,5 +1,9 @@
 const Ship = require('./Ship.js');
 
-test('is a new ship already sunk', ()=>{
+test("a new ship isSunk?", ()=>{  
   expect(new Ship(5).isSunk()).toBe(false);
 });
+
+// test("a ship that is hit registers damage", ()=>{
+//   expect(new Ship(5).hit(4)).toBe(true);
+// });

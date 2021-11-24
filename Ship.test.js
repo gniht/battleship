4c421027f1440ac89f6a6779a92ff28d28508be7
gniht <jasonthomasjat@gmail.com>
2021-11-24 12:04:39 -0800

    testing ship damage tracking

--- Ship.test.js
+++ Ship.test.js
@@ -9,4 +9,8 @@ test("a ship that is hit registers damage", ()=>{
   expect(testShip.hit(1)).toBe(true);  
 });
 
-
+test("a ship that has taken all hits is sunk", ()=>{
+  const testShip = new Ship(1);
+  testShip.hit(1);
+  expect(testShip.isSunk()).toBe(true);  
+});
